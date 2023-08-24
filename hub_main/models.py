7f46817a637dcf7ab7d1c59dbc03165fb75da0f3
linx02 <elvius.linus@gmail.com>
2023-08-24 18:01:32 +0200

    Define database architecture

---
+++ hub_main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, blank=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project_post')
+    created_on = models.DateField(auto_now_add=True)
+    description = models.TextField()
+    likes = models.ManyToManyField(User, related_name='liked_posts')
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.name
+    
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.user}'
+
