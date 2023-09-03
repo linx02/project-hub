5b9c717e1c3e9ee5502c4e3382355caf2d9b52c4
linx02 <elvius.linus@gmail.com>
2023-09-03 16:55:17 +0200

    Add logout functionality

--- members/views.py
+++ members/views.py
@@ -17,3 +17,8 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')
