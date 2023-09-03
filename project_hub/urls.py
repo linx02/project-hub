df809d7a691389ddd0a3deb0b10fa58bea661a38
linx02 <elvius.linus@gmail.com>
2023-09-03 11:26:50 +0200

    Add login functionality

--- project_hub/urls.py
+++ project_hub/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hub_main.urls'), name='hub_main_urls')
+    path('', include('hub_main.urls'), name='hub_main_urls'),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls') ),
 ]
