5885fde24cd0be6de4a441fc3626ed40dece1d55
linx02 <elvius.linus@gmail.com>
2023-09-07 15:56:51 +0200

    Fix typo in ALLOWED_HOSTS

--- project_hub/settings.py
+++ project_hub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['project-hub-0ecc1af6b9be.herokuapp.com/']
+ALLOWED_HOSTS = ['project-hub-0ecc1af6b9be.herokuapp.com']
 
 
 # Application definition
