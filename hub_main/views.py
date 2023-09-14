0db9958dddb3f7cf6c912bc2c4c3904e55ecb69d
linx02 <elvius.linus@gmail.com>
2023-09-14 21:05:50 +0200

    Add comments to hub_main views.py

--- hub_main/views.py
+++ hub_main/views.py
@@ -7,18 +7,19 @@ from django.db.models import Count
 import cloudinary.uploader
 import os
 
-# Create your views here.
-
 def home(request):
 
+    # Get posts for hall of fame sections
     hof_posts = Post.objects.all().annotate(like_count=Count('likes'))
     hof_posts = hof_posts.order_by('-like_count')
     hof_posts = list(hof_posts)
 
+    # Get posts for recently added section
     recent_posts = Post.objects.all().order_by('created_on')
     recent_posts = list(recent_posts)
     recent_posts.reverse()
 
+    # Context to be passed
     context = {
         'hof_post_1' : hof_posts[0],
         'hof_post_2' : hof_posts[1],
@@ -31,13 +32,19 @@ def home(request):
 
 def project_details(request, slug):
 
+    # Get post to be viewed
     post = get_object_or_404(Post.objects.all(), slug=slug)
+
+    # Get comments for post
     comments = post.comments.all().order_by('created_on')
+
+    # Get user liked status
     liked = False
     author_posts = Post.objects.filter(author=post.author)
     if post.likes.filter(id=request.user.id).exists():
         liked = True
 
+    # Context to be passed
     context = {
         'post' : post,
         'comments' : comments,
@@ -49,27 +56,35 @@ def project_details(request, slug):
     return render(request, 'project_details.html', context)
 
 def project_submission(request):
+
+    # Redirect home if user is not authenticated
     if not request.user.is_authenticated:
         return redirect('home')
     
+    # Runs on submission
     if request.method == 'POST':
 
+        # Get title and generate slug
         title = request.POST.get('title')
         slug = slugify(title)
 
+        # Generate screenshot if 'Generate from link' button is toggled
         if 'generate_link' in request.POST:
             image_file = f'https://image.thum.io/get/width/1600/crop/700/auth/{os.environ.get("THUMIO_AUTH")}/{request.POST["live_link"]}'
             upload_result = cloudinary.uploader.upload(image_file)
             image = upload_result['secure_url']
 
+        # Get file if file was uploaded
         elif request.FILES:
             image_file = request.FILES.get('image')
             upload_result = cloudinary.uploader.upload(image_file)
             image = upload_result['secure_url']
         
+        # Use default image if none was provided
         else:
             image = None
         
+        # Create and save post in db
         posts_table = Post(title=request.POST['title'],
                            author=request.user,
                            Category_id=request.POST['category'],
@@ -80,42 +95,55 @@ def project_submission(request):
                            slug=slug)
         posts_table.save()
 
+        # Provide feedback to user
         messages.success(request, 'Your project has been posted. Thank you for your submission!')
         return redirect('profile_page')
     else:
+        # Provide form
         return render(request, 'project_submission.html')
 
 def project_update(request, post_id):
+
+    # Redirect home if user is not authenticated
     if not request.user.is_authenticated:
         return redirect('home')
 
+    # Get post to update
     post_to_update = get_object_or_404(Post, id=post_id)
 
+    # Redirect home if user is not author of post
     if post_to_update.author != request.user:
         return redirect('home')
 
+    # Runs on submission
     if request.method == 'POST':
+
+        # Get new title and generate new slug
         title = request.POST.get('title')
         slug = slugify(title)
 
-
+        # Get new data
         post_to_update.title=title
         post_to_update.Category_id=request.POST.get('category')
         post_to_update.description=request.POST.get('description')
 
+        # Handle 'Generate from link' button toggled
         if 'generate_link' in request.POST:
             image_file = f'https://image.thum.io/get/width/1600/crop/700/auth/{os.environ.get("THUMIO_AUTH")}/{request.POST["live_link"]}'
             upload_result = cloudinary.uploader.upload(image_file)
             image = upload_result['secure_url']
 
+        # Handle file uploaded
         elif request.FILES:
             image_file = request.FILES.get('image')
             upload_result = cloudinary.uploader.upload(image_file)
             image = upload_result['secure_url']
         
+        # Handle none provided
         else:
             image = None
 
+        # Create new record and save to db
         post_to_update.image=image
         post_to_update.live_link=request.POST.get('live_link')
         post_to_update.github_repo_link=request.POST.get('github_repo_link')
@@ -123,10 +151,12 @@ def project_update(request, post_id):
 
         post_to_update.save()
 
+        # Provide user feedback
         messages.success(request, 'Post has been updated.')
         return redirect('profile_page')
     
     else:
+        # Provide data to be pre-filled to inputs when accessing page
         context = {
             'title': post_to_update.title,
             'category': post_to_update.Category_id,
@@ -139,9 +169,12 @@ def project_update(request, post_id):
         return render(request, 'project_update.html', context)
 
 def profile_page(request):
+
+    # Redirect home if user is not authenticated
     if not request.user.is_authenticated:
         return redirect('home')
     
+    # Context to be passed
     context = {
         'posts' : Post.objects.filter(author=request.user),
         'post_count' : Post.objects.filter(author=request.user).count(),
@@ -150,14 +183,19 @@ def profile_page(request):
     return render(request, 'profile_page.html', context)
 
 def delete_post(request, post_id):
+
+    # Redirect home if user is not authenticated
     if not request.user.is_authenticated:
         return redirect('home')
 
+    # Get post to delete
     post = get_object_or_404(Post, id=post_id)
 
+    # Redirect home if user is not author of post
     if post.author != request.user:
         return redirect('home')
 
+    # Delete post and provide user feedback
     if request.method == 'POST':
         post.delete()
         messages.success(request, 'Post has been deleted')
@@ -165,18 +203,25 @@ def delete_post(request, post_id):
     return redirect('profile_page')
 
 def post_comment(request, post_id):
+
+    # Redirect home if user is not authenticated
     if not request.user.is_authenticated:
         return redirect('home')
 
+    # Runs on submission
     if request.method == 'POST':
+
+        # Get post to be commented on
         post = get_object_or_404(Post, id=post_id)
         
+        # Deny if comment is empty
         if request.POST.get('comment_body').strip() == '':
             messages.success(request, 'Empty comments not allowed')
 
             redirect_url = reverse('project_details', kwargs={'slug': post.slug})
             return redirect(redirect_url)
 
+        # Create record, save to db and provide user feedback
         comment = Comment(
             body=request.POST['comment_body'],
             user=request.user,
@@ -190,15 +235,20 @@ def post_comment(request, post_id):
         return redirect(redirect_url)
     
 def delete_comment(request, comment_id, post_id):
+
+    # Redirect home if user is not authenticated
     if not request.user.is_authenticated:
         return redirect('home')
 
+    # Get comment to be deleted, post to redirect to when done
     comment = get_object_or_404(Comment, id=comment_id)
     post = get_object_or_404(Post, id=post_id)
 
+    # Redirect home if user is not author of comment
     if comment.user != request.user:
         return redirect('home')
 
+    # Delete comment and provide user feedback
     if request.method == 'POST':
         comment.delete()
         messages.success(request, 'Comment has been deleted')
@@ -209,15 +259,17 @@ def delete_comment(request, comment_id, post_id):
     
 def browse_project(request, pp, sort_by):
     
+    # Handle sort by a-z
     if sort_by == 'a-z':
         posts = Post.objects.all().order_by('title')
         posts = list(posts)
         posts.reverse()
         return render(request, 'browse_project.html', {'posts': Post.objects.filter(Category_id=pp).order_by('title'), 'category' : Category.objects.get(id=pp)})
+    
+    # Handle sort by recently added
     elif sort_by == 'recently-added':
         posts = Post.objects.filter(Category_id=pp).order_by('-created_on')
         posts = list(posts)
-        posts.reverse()
         context = {
             'posts' : posts,
             'category' : Category.objects.get(id=pp)
@@ -225,6 +277,7 @@ def browse_project(request, pp, sort_by):
 
         return render(request, 'browse_project.html', context)
 
+    # Handle sort by top rated
     elif sort_by == 'top-rated':
         posts = Post.objects.filter(Category_id=pp).annotate(like_count=Count('likes'))
         posts = posts.order_by('-like_count')
@@ -235,33 +288,46 @@ def browse_project(request, pp, sort_by):
 
         return render(request, 'browse_project.html', {'posts': posts, 'category' : Category.objects.get(id=pp)})
     
+    # Render page
     else:
         return render(request, 'browse_project.html', {'posts': Post.objects.filter(Category_id=pp), 'category' : Category.objects.get(id=pp)})
 
 
 def like_post(request, post_id):
+
+    # Redirect home if user is not authenticated
     if not request.user.is_authenticated:
         return redirect('home')
     
+    # Get post to like
     post = get_object_or_404(Post, id=post_id)
 
+    # Like if not liked, unlike if already liked
     if post.likes.filter(id=request.user.id).exists():
         post.likes.remove(request.user)
     else:
         post.likes.add(request.user)
 
+    # Render project page
     redirect_url = reverse('project_details', kwargs={'slug': post.slug})
     return redirect(redirect_url)
 
 def view_profile(request, username):
+
+    # Get profile to view
     profile = get_object_or_404(User, username=username)
+
+    # Context to be passed
     context = {
         'profile' : profile,
         'posts' : Post.objects.filter(author=profile),
         'post_count' : Post.objects.filter(author=profile).count(),
     }
 
+    # Render page
     return render(request, 'view_profile.html', context)
 
 def custom_404_page(request, *args, **kwargs):
+
+    # Render custom 404 page
     return render(request, '404.html', status=404)
